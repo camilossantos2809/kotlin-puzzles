b01fc57670f42539b0da3909dec18e1c53f263bb
Camilo Santos <camilossantos.2809@gmail.com>
2024-12-07 16:52:02 -0300

    Day1 refactor (#8)
    * use abs()
    
    * use more functional style
    
    * replace with sumof

--- src/main/kotlin/year2024/day1/year2024Day1.kt
+++ src/main/kotlin/year2024/day1/year2024Day1.kt
@@ -3,33 +3,21 @@ package year2024.day1
 import parseTextToLists
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.math.abs
 
 
 fun calcTotalDistance(lists: Pair<List<String>, List<String>>): Int {
     val (leftList, rightList) = lists
-    val sortedLeftList = leftList.sorted()
-    val sortedRightList = rightList.sorted()
-    var sum = 0
-    for (i in sortedLeftList.indices) {
-        var diff: Int
-        val left = sortedLeftList[i].toInt()
-        val right = sortedRightList[i].toInt()
-        diff = if (left > right) {
-            left - right
-        } else {
-            right - left
-        }
-        sum += diff
+    return leftList.sorted().zip(rightList.sorted()).sumOf { (left, right) ->
+        abs(left.toInt() - right.toInt())
     }
-    return sum
 }
-
 
 fun calculateSimilarity(lists: Pair<List<String>, List<String>>): Int {
     val (leftList, rightList) = lists
 //    implementar primeiro busca sequencial para ver se o resultado ficará correto, depois tentar fazer cache com busca indexada
-    return leftList.fold(0) { acc, left ->
-        left.toInt() * rightList.count { it.contains(left) } + acc
+    return leftList.sumOf {  left ->
+        left.toInt() * rightList.count { it.contains(left) }
     }
 }
 
