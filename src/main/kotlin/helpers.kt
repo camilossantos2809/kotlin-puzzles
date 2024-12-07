1871d91d108df7609caa3aada6f670ebaf53f957
camilosantos <camilossantos.2809@gmail.com>
2024-12-07 11:56:46 -0300

    move helper function

---
+++ src/main/kotlin/helpers.kt
@@ -0,0 +1,10 @@
+ fun parseTextToLists(input: List<String>): Pair<List<String>, List<String>> {
+    val leftList = emptyList<String>().toMutableList()
+    val rightList = emptyList<String>().toMutableList()
+    input.map { line ->
+        val (left, right) = line.split("   ")
+        leftList.add(left)
+        rightList.add(right)
+    }
+    return Pair(leftList, rightList)
+}
