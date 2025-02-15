c18e5bb05d781d993dfd68c21c64978bf3237652
camilosantos <camilossantos.2809@gmail.com>
2025-02-15 19:19:42 -0300

    solving day 3, part 2

--- src/commonMain/kotlin/year2024/day3/year2024Day3.kt
+++ src/commonMain/kotlin/year2024/day3/year2024Day3.kt
@@ -6,9 +6,18 @@ fun extractMulValues(text: String): List<String> {
     return matches.map { it.value }.toList()
 }
 
+// remove all text between 'don't()' and 'do()' before extracting mul values
+fun extractedFilteredMulValues(text: String): List<String> {
+    val regex = Regex("don't\\(\\).*?do\\(\\)")
+    val filteredText = text.replace(regex, "")
+    return extractMulValues(filteredText)
+}
+
 fun calcMulValue(mulValue: String): Long {
     val (first, second) = mulValue.replace("mul(", "").replace(")", "").split(",")
     return first.toLong() * second.toLong()
 }
 
 fun sumAllMulValues(text: String): Long = extractMulValues(text).sumOf { calcMulValue(it) }
+
+fun sumAllMulValuesFiltered(text: String): Long = extractedFilteredMulValues(text).sumOf { calcMulValue(it) }
--- src/commonTest/kotlin/year2024/day3/Year2024Day3KtTest.kt
+++ src/commonTest/kotlin/year2024/day3/Year2024Day3KtTest.kt
@@ -5,6 +5,8 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 const val inputPath = "inputs/input-3.txt"
+const val exampleDay1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+const val exampleDay2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 class Year2024Day3KtTest {
 
@@ -16,13 +18,13 @@ class Year2024Day3KtTest {
 
     @Test
     fun `extractMulValues - should return a list of mul values`() {
-        val result = extractMulValues("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+        val result = extractMulValues(exampleDay1)
         assertEquals(listOf("mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"), result)
     }
 
     @Test
     fun `sumAllMulValues - Example from advent of code day 1`() {
-        val result = sumAllMulValues("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+        val result = sumAllMulValues(exampleDay1)
         assertEquals(161, result)
     }
 
@@ -31,5 +33,24 @@ class Year2024Day3KtTest {
         val input = FileHelper.readLines(inputPath)
         val result = sumAllMulValues(input.joinToString())
         assertEquals(173529487, result)
+    }
+
+    @Test
+    fun `extractedFilteredMulValues - should return a list of mul values that are not between 'don't' and 'do'`() {
+        val result = extractedFilteredMulValues(exampleDay2)
+        assertEquals(listOf("mul(2,4)", "mul(8,5)"), result)
+    }
+
+    @Test
+    fun `sumAllMulValuesFiltered - Example from advent of code day 2`() {
+        val result = sumAllMulValuesFiltered(exampleDay2)
+        assertEquals(48, result)
+    }
+
+    @Test
+    fun `part 2 - Full input - should always return the correct answer to advent of code`() {
+        val input = FileHelper.readLines(inputPath)
+        val result = sumAllMulValuesFiltered(input.joinToString())
+        assertEquals(99532691, result)
     }
 }
